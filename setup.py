50a1e27bd4b1aea6dd13af090f5b3077cb6ac7ec
MMesch <MMesch@users.noreply.github.com>
2016-07-22 12:48:11 +0200

    new setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import sys
 import os
 import re
-from setuptools import setup, find_packages
-from distutils.command.build import build as _build
+
+try:
+    import numpy  # @UnusedImport # NOQA
+except:
+    msg = ("No module named numpy. Install numpy before SHTOOLS")
+    raise ImportError(msg)
+
+from setuptools import find_packages
+from numpy.distutils.core import setup, Extension
+from numpy.distutils.misc_util import Configuration
 from subprocess import CalledProcessError, check_output, check_call
 from multiprocessing import cpu_count
 
@@ -38,27 +47,26 @@ def get_version():
     return version
 
 
-class build(_build):
-    description = "run make command from setup.py and than usual build"
+def configuration(parent_package="", top_path=None):
+    """
+    builds the Fortran library only. The python part is
+    built later.
+    """
+    # Ultimately, the Fortran part should be built by numpy as well such that
+    # the same libraries are used to build the fortran and python part
+    config = Configuration("", parent_package, top_path)
 
-    def run(self):
-        make_fortran = ['make', 'fortran']
+    # build Fortran library using the makefile
+    make_fortran = ['make', 'fortran']
 
-        try:
-            make_fortran.append('-j%d' % cpu_count())
-        except:
-            pass
+    try:
+        make_fortran.append('-j%d' % cpu_count())
+    except:
+        pass
 
-        check_call(make_fortran)
+    check_call(make_fortran)
 
-        if sys.version_info.major == 3:
-            make_python = ['make', 'python3']
-        else:
-            make_python = ['make', 'python2']
-
-        check_call(make_python)
-
-        _build.run(self)
+    return config
 
 
 CLASSIFIERS = [
@@ -80,22 +88,52 @@ CLASSIFIERS = [
 ]
 
 
+KEYWORDS = ['Spherical Harmonics', 'Wigner Symbols']
+
+
+INSTALL_REQUIRES = [
+    'future>=0.12.4',
+    'numpy>=1.0.0',
+    # 'scipy>=0.9.0',
+    # 'matplotlib>=1.1.0',
+    'setuptools']
+
+# configure python extension to be compiled with f2py
+
+# Absoft f95 flags:
+# F95FLAGS = ['m64', 'O3', 'YEXT_NAMES=LCS', 'YEXT_SFX=_', 'fpic',
+#             'speed_math=10']
+# gfortran flags:
+F95FLAGS = ['-m64', '-fPIC', '-O3', '-ffast-math']
+#fftw3_libdir = '/usr/lib/x86_64-linux-gnu/'
+
+ext = Extension(name='pyshtools._SHTOOLS',
+                include_dirs=['modules'],
+                libraries=['SHTOOLS', 'fftw3', 'm', 'lapack', 'blas'],
+                library_dirs=['/usr/local/lib', 'lib'],
+                extra_link_args=F95FLAGS,
+                extra_compile_args=F95FLAGS,
+                sources=['src/pyshtools.pyf', 'src/PythonWrapper.f95'])
+
+
 metadata = dict(
     name='pyshtools',
     version=get_version(),
     description='SHTOOLS - Tools for working with spherical harmonics',
     url='http://shtools.ipgp.fr',
-    download_url='https://github.com/SHTOOLS/SHTOOLS/releases',
+    download_url='https://github.com/SHTOOLS/SHTOOLS/zipball/master',
     author='Mark Wieczorek, Matthias Meschede et al.',
     license='BSD',
+    keywords=KEYWORDS,
+    install_requires=INSTALL_REQUIRES,
     platforms='OS Independent',
     packages=find_packages(),
     package_data={'': ['doc/*.doc', '*.so']},
     include_package_data=True,
-    cmdclass={
-        'build': build,
-    },
+    configuration=configuration,
     classifiers=CLASSIFIERS,
+    ext_modules=[ext]
 )
+
 
 setup(**metadata)
