25be25c15dd614ecb83c260e654b6910177d7993
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2016-08-30 11:40:51 +0200

    Bump version to 3.4, rebuild docs, place html notebooks in www/notebooks

--- pyshtools/__init__.py
+++ pyshtools/__init__.py
@@ -38,7 +38,7 @@ from __future__ import absolute_import as _absolute_import
 from __future__ import division as _division
 from __future__ import print_function as _print_function
 
-__version__ = '3.3.1'
+__version__ = '3.4'
 __author__ = 'SHTOOLS developers'
 
 import os as _os
