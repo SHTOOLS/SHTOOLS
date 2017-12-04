ae6732583b92af312437098df3800a23ce31e8a8
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2017-12-04 15:15:28 +0100

    Change ISRELEASED to True

--- pyshtools/__init__.py
+++ pyshtools/__init__.py
@@ -37,7 +37,7 @@ from __future__ import absolute_import as _absolute_import
 from __future__ import division as _division
 from __future__ import print_function as _print_function
 
-__version__ = '4.2'
+__version__ = '4.1.2'
 __author__ = 'SHTOOLS developers'
 
 import os as _os
