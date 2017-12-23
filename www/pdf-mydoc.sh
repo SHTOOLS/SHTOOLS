08c9fe2b33c96915618b617f63ed0e3c358f0544
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2017-12-24 00:30:36 +0100

    Fix some problems with pdf generation. MathJax doesn't work, and some pages are not correctly cross-referenced

--- www/pdf-mydoc.sh
+++ www/pdf-mydoc.sh
@@ -5,10 +5,10 @@ kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
 echo "Building PDF-friendly HTML site for Mydoc ...";
-bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_pdf.yml;
+jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_pdf.yml;
 echo "done";
 
 echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/mydoc.pdf;
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/shtools.pdf;
 
 echo "Done. Look in the pdf directory to see if it printed successfully."
