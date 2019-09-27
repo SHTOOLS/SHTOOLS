0c53a0d0bad35c6024d2261a41e9f3a82542f07e
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2019-09-27 23:48:35 +0200

    fix

--- setup.py
+++ setup.py
@@ -223,7 +223,8 @@ def configuration(parent_package='', top_path=None):
 
     # Fortran compilation
     kwargs_win = {
-        'extra_f90_compile_args': ['-fno-underscoring']
+        'extra_f90_compile_args': ['-funderscoring'],
+        'extra_compile_args': ['-funderscoring']
     }
 
     if sys.platform == 'win32':
