87b7e9e7ce135c61e2cb42350e7b64da609f5ed8
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2019-09-27 13:59:37 +0200

    fix

--- setup.py
+++ setup.py
@@ -222,10 +222,14 @@ def configuration(parent_package='', top_path=None):
     print('searching SHTOOLS in:', libdir)
 
     # Fortran compilation
+
+    kwargs_win = {
+        'extra_f90_compile_args' = ['-DLAPACK_UNDERSCORE']
+    }
+
     if sys.platform == 'win32':
         config.add_library('SHTOOLS', sources=sources,
-                           extra_compile_args = '-DLAPACK_UNDERSCORE')
-                           # extra_f90_compile_args = '-DLAPACK_UNDERSCORE'
+                           **kwargs_win)
     else:
         config.add_library('SHTOOLS', sources=sources)
 
