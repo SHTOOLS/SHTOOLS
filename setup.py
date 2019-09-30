d94c21d30abdf10bba2d2bf29b409786c0a91d3a
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2019-09-30 14:02:53 +0200

    test

--- setup.py
+++ setup.py
@@ -253,8 +253,8 @@ def configuration(parent_package='', top_path=None):
     dict_append(kwargs, **blas_info)
     dict_append(kwargs, **lapack_info)
 
-    if sys.platform == 'win32':
-        kwargs['runtime_library_dirs'] = []
+    # if sys.platform == 'win32':
+    #    kwargs['runtime_library_dirs'] = []
 
     print('**** ==== KWARGS ==== ****')
     print(kwargs)
