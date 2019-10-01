7e1db4f9598fe197960fa1033536d45643331479
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2019-10-01 15:53:56 +0200

    test

--- setup.py
+++ setup.py
@@ -256,8 +256,6 @@ def configuration(parent_package='', top_path=None):
     # if sys.platform == 'win32':
     #    kwargs['runtime_library_dirs'] = []
 
-    kwargs = {'libraries': ['SHTOOLS', 'fftw3', 'blas', 'lapack'], 'include_dirs': ['build\\temp.win-amd64-3.6', 'C:\\projects\\build-shtools\\fftw', 'C:\\projects\\build-shtools\\lapack', 'c:\\python36-x64\\include'], 'library_dirs': ['build\\temp.win-amd64-3.6', 'C:\\projects\\build-shtools\\fftw', 'C:\\projects\\build-shtools\\lapack'], 'define_macros': [('SCIPY_FFTW3_H', None), ('NO_ATLAS_INFO', 1)], 'runtime_library_dirs': []}
-
     config.add_extension('pyshtools._SHTOOLS',
                          sources=['src/pyshtools.pyf',
                                   'src/PythonWrapper.f95'],
