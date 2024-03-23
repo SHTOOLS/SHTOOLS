c79a5065f57bf5067c808bac724a7d3a6f9b137a
Mark Wieczorek <mark.wieczorek@ipgp.fr>
2024-03-23 22:17:15 +0100

    Add orbital elements of asteroids

--- pyshtools/constants/Ceres.py
+++ pyshtools/constants/Ceres.py
@@ -98,5 +98,44 @@ angular_velocity = _Constant(
     'Dawn radiometric tracking and optical data, Icarus, 299, 411-429, '
     'doi:10.1016/j.icarus.2017.08.005.')
 
+orbit_semimajor_axis = _Constant(
+    abbrev='orbit_semimajor_axis_ceres',
+    name='Semimajor axis of the orbit of (1) Ceres about the Sun',
+    value=2.767254360873952,
+    unit='au',
+    uncertainty=1.0197E-11,
+    reference='JPL Solar System Dynamics, https://ssd.jpl.nasa.gov, '
+    'solution date: 2021-Apr-13 11:04:44')
+
+orbit_eccentricity = _Constant(
+    abbrev='orbit_eccentricity_ceres',
+    name='Eccentricity of the orbit of (1) Ceres about the Sun',
+    value=0.0789125317658808,
+    unit='',
+    uncertainty=4.7845E-12,
+    reference='JPL Solar System Dynamics, https://ssd.jpl.nasa.gov, '
+    'solution date: 2021-Apr-13 11:04:44')
+
+orbit_inclination = _Constant(
+    abbrev='orbit_inclination_ceres',
+    name='Inclination of the orbit of (1) Ceres about the Sun, with respect '
+    'to the mean ecliptic and equinox of J2000',
+    value=10.5868796009696,
+    unit='degrees',
+    uncertainty=4.6112E-9,
+    reference='JPL Solar System Dynamics, https://ssd.jpl.nasa.gov, '
+    'solution date: 2021-Apr-13 11:04:44')
+
+orbit_angular_velocity = _Constant(
+    abbrev='orbit_angular_velocity_ceres',
+    name='Orbital angular velocity of (1) Ceres about the Sun',
+    value=2 * _np.pi / (1681.403889244621 * 24 * 60 * 60),
+    unit='rad / s',
+    uncertainty=2 * _np.pi / (24 * 60 * 60) * 9.2941E-9 / 1681.403889244621**2,
+    reference='JPL Solar System Dynamics, https://ssd.jpl.nasa.gov, '
+    'solution date: 2021-Apr-13 11:04:44')
+
 __all__ = ['gm', 'mass', 'mean_radius', 'r', 'volume_equivalent_radius',
-           'volume', 'gravity_mean_radius', 'mean_density', 'angular_velocity']
+           'volume', 'gravity_mean_radius', 'mean_density',
+           'angular_velocity', 'orbit_semimajor_axis', 'orbit_eccentricity',
+           'orbit_inclination', 'orbit_angular_velocity']
